25c375dcdbb6c84a0395d3337c17df2a59a7586b
cdom <dominguerilla@gmail.com>
2021-05-14 11:35:00 -0400

    Add BulkHazardActivator and Hazard.Activate()
    Now the cacti on the path to the gate are inactive, but waiting...

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,23 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Hazard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isActivated;
+    public UnityEvent onActivation = new UnityEvent();
+    public UnityEvent onDeactivation = new UnityEvent();
+
+    private void Start()
     {
-        
+        if (isActivated) onActivation.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Activate()
     {
-        
+        if (isActivated) return;
+        isActivated = true;
+        onActivation.Invoke();
+    }
+
+    public void Deactivate()
+    {
+        if (!isActivated) return;
+        isActivated = false;
+        onDeactivation.Invoke();
+    }
+
+    public void ActivateAllChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+
+    public void DeactivateAllChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!isActivated) return;
         Respawnable respawnable = other.GetComponent<Respawnable>();
         if (respawnable)
         {
