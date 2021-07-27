755bd5531b0da2e888e613d6475ee4155d608503
Carlos Dominguez <dominguerilla@gmail.com>
2021-07-26 22:09:02 -0400

    Add FSM tracking MENTAL STATE

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Bolt;
 
 [RequireComponent(typeof(Damageable))]
 public class PlayerStatus : MonoBehaviour
@@ -19,7 +20,7 @@ public class PlayerStatus : MonoBehaviour
 
     void OnProjectileHit(Projectile projectile)
     {
-        Debug.Log($"{gameObject.name} hit by { projectile.gameObject.name }!");
+        CustomEvent.Trigger(this.gameObject, "OnProjectileHit", projectile);
     }
 
 }
