af2f8ecba2840fa9c1fb5b4f619df1c83575387b
Carlos Dominguez <dominguerilla@gmail.com>
2021-08-25 18:56:21 -0400

    Change UI color based on race

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -8,6 +8,7 @@ using UnityEngine.Events;
 public class Weapon : ItemComponent
 {
     public UnityEvent onUpgrade = new UnityEvent();
+    public UnityEvent onStatusApplied = new UnityEvent();
 
     [SerializeField] float soundDelay = 0f;
    
@@ -21,6 +22,7 @@ public class Weapon : ItemComponent
     bool _isAttacking = false;
     Camera mainCam;
     float cooldownFactor = 1.0f;
+    Color weaponColor = Color.white;
 
     private void Start()
     {
@@ -30,6 +32,23 @@ public class Weapon : ItemComponent
     public override void Use()
     {
         if(!_isAttacking) StartCoroutine(Attack());
+    }
+
+    public float GetAttackCooldown()
+    {
+        return baseAttackCooldown * cooldownFactor;
+    }
+    public void ApplyStatus(Status status)
+    {
+        this.weaponColor = status.raceStatus.weaponColor;
+        this.cooldownFactor = CalculateCooldown(status);
+        projectilePool.ApplyStats(status);
+        onStatusApplied.Invoke();
+    }
+
+    public Color GetWeaponColor()
+    {
+        return this.weaponColor;
     }
 
     IEnumerator Attack()
@@ -53,19 +72,12 @@ public class Weapon : ItemComponent
         projectilePool.Launch(launchPosition + mainCam.transform.forward, mainCam.transform.rotation);
     }
 
-    public float GetAttackCooldown()
-    {
-        return baseAttackCooldown * cooldownFactor;
-    }
-    public void ApplyStatus(Status status)
-    {
-        this.cooldownFactor = CalculateCooldown(status);
-        projectilePool.ApplyStats(status);
-    }
-
     float CalculateCooldown(Status status)
     {
         float DEX = status.raceStatus.DEX;
         return 1 / DEX;
     }
+
+
+
 }
