5366d829271dc544e48dd234a96d8108da39f7c6
Carlos Dominguez <dominguerilla@gmail.com>
2021-08-31 11:58:50 -0400

    Update project name, change character portraits based on form

--- Assets/Scripts/StatusUpdater.cs
+++ Assets/Scripts/StatusUpdater.cs
@@ -5,11 +5,39 @@ using UnityEngine.UI;
 public class StatusUpdater : MonoBehaviour
 {
     [SerializeField] PlayerStatus playerStatus;
+    [Header("UI Elements")]
     [SerializeField] Text statusText;
+    [SerializeField] Image characterPortrait;
+
+    [Header("Character Portraits")]
+    [SerializeField] Sprite boneForm;
+    [SerializeField] Sprite ghostForm;
+    [SerializeField] Sprite demonForm;
 
     public void UpdateStatusText()
     {
         string raceName = playerStatus.GetRace().name;
         statusText.text = raceName;
+    }
+
+    public void UpdateCharacterPortrait()
+    {
+        RACE race = playerStatus.GetRace().race;
+        switch (race)
+        {
+            case RACE.GHOST:
+                characterPortrait.sprite = ghostForm;
+                break;
+            case RACE.DEMON:
+                characterPortrait.sprite = demonForm;
+                break;
+            case RACE.BONE:
+                characterPortrait.sprite = boneForm;
+                break;
+            default:
+                Debug.LogError($"No character portrait set for race {race.ToString()}");
+                characterPortrait.sprite = boneForm;
+                break;
+        }
     }
 }
