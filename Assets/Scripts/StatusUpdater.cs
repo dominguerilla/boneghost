8db14acd898c364dc83d8198fe12e8fdcec08fa1
Carlos Dominguez <dominguerilla@gmail.com>
2021-08-31 12:42:51 -0400

    Change player chat portraits based on form

--- Assets/Scripts/StatusUpdater.cs
+++ Assets/Scripts/StatusUpdater.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using PixelCrushers.DialogueSystem;
+
+/// <summary>
+/// Used in PlayerStatus.OnRaceChange() to update the Status UI and dialogue character portraits.
+/// </summary>
 public class StatusUpdater : MonoBehaviour
 {
     [SerializeField] PlayerStatus playerStatus;
+    [SerializeField] DialogueActor playerActor;
+
     [Header("UI Elements")]
     [SerializeField] Text statusText;
     [SerializeField] Image characterPortrait;
 
-    [Header("Character Portraits")]
+    [Header("UI Portraits")]
     [SerializeField] Sprite boneForm;
     [SerializeField] Sprite ghostForm;
     [SerializeField] Sprite demonForm;
+
+    [Header("Chat Portraits")]
+    [SerializeField] Sprite boneFormChat;
+    [SerializeField] Sprite ghostFormChat;
+    [SerializeField] Sprite demonFormChat;
 
     public void UpdateStatusText()
     {
@@ -20,7 +32,7 @@ public class StatusUpdater : MonoBehaviour
         statusText.text = raceName;
     }
 
-    public void UpdateCharacterPortrait()
+    public void UpdateUIPortrait()
     {
         RACE race = playerStatus.GetRace().race;
         switch (race)
@@ -35,8 +47,29 @@ public class StatusUpdater : MonoBehaviour
                 characterPortrait.sprite = boneForm;
                 break;
             default:
-                Debug.LogError($"No character portrait set for race {race}");
+                Debug.LogError($"No UI portrait set for race {race}");
                 characterPortrait.sprite = boneForm;
+                break;
+        }
+    }
+
+    public void UpdateCharacterPortrait()
+    {
+        RACE race = playerStatus.GetRace().race;
+        switch (race)
+        {
+            case RACE.GHOST:
+                playerActor.spritePortrait = ghostFormChat;
+                break;
+            case RACE.DEMON:
+                playerActor.spritePortrait = demonFormChat;
+                break;
+            case RACE.BONE:
+                playerActor.spritePortrait = boneFormChat;
+                break;
+            default:
+                Debug.LogError($"No character portrait set for race {race}");
+                playerActor.spritePortrait = boneFormChat;
                 break;
         }
     }
